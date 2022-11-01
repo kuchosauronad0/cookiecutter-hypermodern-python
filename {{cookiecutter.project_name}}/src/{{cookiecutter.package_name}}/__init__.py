b4da12284e58fb7dc19ea0bad0c898afa4813604
kuchosauronad0 <22005492+kuchosauronad0@users.noreply.github.com>
2022-10-31 19:53:06 -0700

    feature: add logging

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -1,1 +1,10 @@
 """{{cookiecutter.friendly_name}}."""
+from importlib.metadata import version
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+try:
+    __version__ = version(__name__)
+except Exception:
+    logger.error("Error determining version from importlib.metadata")
